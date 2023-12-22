0671a2eaa8c573e2a6daf4bfe24f41c1811db268
Sakshi Jadaun <sakshijadaun080@gmail.com>
2023-12-23 00:23:56 +0530

    initial commit

---
+++ JsEXercise1/index.js
@@ -0,0 +1,33 @@
+//Q1. Write a function that takes two numbers as arguments and returns their sum.
+
+// function sum(num1,num2){
+//     return (num1+num2);
+// }
+// let result = sum(1,2);
+// console.log(result);
+
+//Q2. Write a function that takes a string as an argument and returns its length.
+
+// function stringLength(str){
+//     let length = str.length;
+//     return length;
+// }
+// console.log(stringLength("SakshiJadaun"));
+
+//Q3. Write a program that takes two numbers and displays their sum, difference, product, and quotient.
+ 
+// const num1 = Number(10); //Use number constructor which made them numeric values
+// const num2 = Number(20);
+
+// const sum = num1+num2;
+// const sub = num1-num2;
+// const mul = num1*num2;
+// const div = num1%num2;
+
+// console.log(`sum : ${sum}`);
+// console.log(`sub : ${sub}`);
+// console.log(`mul : ${mul}`);
+// console.log(`div : ${div}`);
+
+//Q4. Write a function that takes two numbers as arguments and returns the larger number.
+
