6e9c6d82ecb530c3481dc7309fc09aa5af6c11e1
Sakshi Jadaun <sakshijadaun080@gmail.com>
2023-12-24 00:38:40 +0530

    final commit

--- JsEXercise1/index.js
+++ JsEXercise1/index.js
@@ -84,3 +84,48 @@
 
 //Q9. Write a program that takes a string and prints out the number of vowels in the string.
 
+// function countVowels(str){
+//     let vowels = ['a','e','i','o','u'];
+//     let count = 0;
+
+//     for(let i=0; i<str.length; i++){
+//         if(vowels.includes(str[i].toLowerCase())){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(countVowels("Sakshi"));
+
+//Q10. Write a function that takes two arrays of integers as arguments and returns an array of the common elements in both arrays, without any duplicates. The returned array should be sorted in ascending order.
+
+function findCommonElements(arr1,arr2){
+
+    //create the empty array to hold the common elements
+    let commonElements = [];
+
+    //Loop through elements on arr1
+    for(let i=0; i<arr1.length; i++){
+
+        //check if the elements in arr2
+        if(arr2.includes(arr1[i])){
+
+        //If the element is in arr2 and not already in commonElements array, add it
+            if(!commonElements.includes(arr1[i])){
+                commonElements.push(arr1[i]);
+            }
+        }
+    }
+
+    //sort the commonElements array in ascending order
+    commonElements.sort((a,b) => a-b);
+
+    //Return the commonElements array
+    return commonElements; 
+}
+
+// Example usage
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [3, 4, 5, 6, 7];
+const commonElements = findCommonElements(arr1, arr2);
+console.log(commonElements); 
