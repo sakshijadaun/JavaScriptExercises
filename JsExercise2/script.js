681320d9be89def5e863d9525da5e7b4f5299cf3
Sakshi Jadaun <sakshijadaun080@gmail.com>
2023-12-28 23:33:25 +0530

    Start Question

---
+++ JsExercise2/script.js
@@ -0,0 +1,13 @@
+//Q1. Write a function called 'greet' that takes a name parameter and logs a greeting message to the console.
+
+function greet(name){
+    console.log(`Hello ${name}`);
+}
+greet("Sakshi");
+
+//Q2. 2. **Write a function called 'getSquare' that takes a number parameter and returns its square.
+
+function getSquare(side){
+    return side**2;
+}
+console.log(getSquare(8));
